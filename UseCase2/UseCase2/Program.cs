a3b9f8caa64b960e5ac83a09b01779ac0aa79f2f
MSRbuddy <meghanadasari222@gmail.com>
2023-01-29 15:32:59 +0530

    Added UC2

---
+++ UseCase2/UseCase2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UseCase2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FULL_TIME = 1;
+            int WAGE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int number = random.Next(0, 2);
+            if(number==FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * WAGE_PER_HOUR;
+            Console.WriteLine("Emplyee Daily Wage is: ");
+        }
+    }
+}
